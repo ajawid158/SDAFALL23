94b15a73c8fbd41b1cdffb614dcbb8d0af9e2712
ajawid158 <ajawid@auaf.edu.af>
2023-10-08 15:01:42 +0430

    ggplot208103

--- ggplot2.R
+++ ggplot2.R
@@ -9,17 +9,22 @@ head(dtTips)
 #Gender
 
 fdtGender=table(dtTips$sex)
+fdtGender
+
 fdtGender=as.data.frame(fdtGender)
+
 fdtGender
 colnames(fdtGender)=c("Gender","Count")
 fdtGender
+
+##we use the FDT of Gender as input for ggplot
 
 g0=ggplot(fdtGender, aes(x="", y=Count, fill=Gender))
 g1=g0+geom_col()+
   coord_polar(theta = "y")+
   theme_void()+
   theme(plot.title = element_text(colour = "blue",
-                                  size = 14, 
+                                  size = 12, 
                                   face = "bold", 
                                   hjust = .5))+
   ggtitle('Gender Distribution of Customers')+
@@ -27,6 +32,7 @@ g1=g0+geom_col()+
             position = position_stack(vjust = .5))+
   scale_fill_manual(values = c('#99FF33', '#BE2A3E'))+
   theme(legend.position = 'bottom')
+
 ggsave('genderDist.png')
 
 g1
