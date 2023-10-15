026c1f0567e491e343497d671bb161fb901a7257
ajawid158 <ajawid@auaf.edu.af>
2023-10-15 15:08:46 +0430

    ggplot21510

--- ggplot2.R
+++ ggplot2.R
@@ -150,11 +150,14 @@ ggsave('boxplotTip.png')
 
 
 
-#Bivariate graphs
+#Bivariate/Multivar graphs
+#2 QL vars
+#1 QL and 1 QNT
+#2 QNTS
 ###+++++++++++++++++++Joint graphs
-#Gender[F, M] and Smoker[Y, N] 
+#Gender[F, M] and Smoker[Y, N] Join barplot for two QL vars
 
-jtable=table(dtTip$sex, dtTip$smoker)
+jtable=table(dtTips$sex, dtTips$smoker)
 jtable=as.data.frame(jtable)
 jtable
 
@@ -165,48 +168,52 @@ ggplot(jtable, aes(x=Var1, y=Freq, fill=Var2))+
         legend.title = element_text(color = 'blue'),
         plot.title = element_text(face = 'bold',
                                   hjust = .5, 
-                                  color='darkgreen'))+
+                                  color='darkgreen'),
+        legend.position = "bottom")+
   ggtitle('Join bar graph of Gender and Smoking')+
   xlab('Gender')+
-  theme(legend.position='bottom')+
   guides(fill=guide_legend('Smoking'))
+
 ggsave('jointBarGenderSmoke.pdf')
 
 
 ####+++++++++++++++++++Joint density 
-##Gender[F, M]  tip
+##Gender[F, M]  tip for on QL and one QNT variables
 
-g0=ggplot(dtTip, aes(x=tip, color=sex))
+g0=ggplot(dtTips, aes(x=tip, color=sex))
 g0+geom_density()+
   theme_replace()+
   scale_color_manual(values = c('red', 'blue'))+
-  xlim(-1,11)+
-  theme(plot.title = element_text(face = 'bold', 
+  xlim(-2,11)+
+  theme(plot.title = element_text(face="bold",
                                   hjust = .5), 
         axis.title.x = element_text(),
         axis.title.y = element_text(), 
-        legend.title = element_text(color='blue'))+
+        legend.title = element_text(color='blue'),
+        legend.position = "bottom")+
   ggtitle('Joint distribution of Tip amount across Gender')+
-  xlab('Tib Amount')+
-  theme(legend.position = 'bottom')
+  xlab('Tib Amount')
+
 ggsave('jointDensity.png')
 
 #####+++++++++++++++++++Ridgeline plot
-#smoker[y, n] tip
+#smoker[y, n] tip 
 #install.packages('ggridges')
 library(ggridges)
 
-ggplot(dtTip, aes(x=tip, y=smoker, fill=smoker))+
-  geom_density_ridges(color=4, 
-                      lwd=.3)+
+ggplot(dtTips, aes(x=tip, y=sex, fill=sex))+
+  geom_density_ridges(color=7, 
+                      lwd=.5)+
   theme_gray()+
   theme(plot.title = element_text(face = 'bold', 
                                   hjust = .5), 
         axis.title.x = element_text(),
         axis.title.y = element_text(), 
-        legend.title = element_text(color='blue'))+
-  ggtitle('Joint dist of Tip and Smoker')+
+        legend.title = element_text(color='blue'),
+        legend.position = "bottom")+
+  ggtitle('Joint dist of Tip and Gender')+
   xlab('Tip Amount')
+
 ggsave('jointDistTipsSmoker.png')
 
 
@@ -214,9 +221,9 @@ ggsave('jointDistTipsSmoker.png')
 #Gender[y,N] tip
 
 
-ggplot(dtTip, aes(x=tip, y=sex, fill=sex))+
+ggplot(dtTips, aes(x=tip, y=sex, fill=sex))+
   geom_boxplot(color=2, 
-               alpha=0.9, 
+               alpha=0.3, 
                outlier.colour = 'blue', 
                linetype=2, 
                lwd=.6)+
@@ -227,31 +234,33 @@ ggplot(dtTip, aes(x=tip, y=sex, fill=sex))+
                                   hjust = .5), 
         axis.title.x = element_text(),
         axis.title.y = element_text(), 
-        legend.title = element_text(color='blue'))+
+        legend.title = element_text(color='blue'), 
+        legend.position = "bottom")+
   ggtitle('Joint dist of Tip and Gender')+
   xlab('Tip Amount')+
   ylab('Gender')+
   xlim(-1,11)
 ggsave('jointboxplot.png')
 
-######+++++++++++++++++++boxplot with points
+######+++++++++++++++++++boxplot with points##not part of ggplot
 
-boxplot(dtTip$tip, col = 'white', horizontal = T)
-stripchart(dtTip$tip, 
-           method = 'jitter', 
+boxplot(dtTips$tip, col = 'white', horizontal = T)
+stripchart(dtTips$tip, 
+           method = 'jitter',
            pch=1, 
            col=4, 
            add = TRUE)
 
+help("stripchart")
 ######+++++++++++++++++++Joint boxplot
 ##Gender[y,n] tip
 
 boxplot(tip~sex,
-        data = dtTip,
+        data = dtTips,
         col='white',
         horizontal = T)
 stripchart(tip~sex,
-           data = dtTip,
+           data = dtTips,
            method = 'jitter', 
            pch=19, 
            col=2:4,
@@ -264,54 +273,58 @@ ggsave('jointboxplotwithpoints.pdf')
 library(ggbeeswarm)
 #smoker[y, n] tip
 
-ggplot(dtTip, aes(x=smoker, y=tip, color=smoker))+
-  geom_beeswarm(cex=1)
+ggplot(dtTips, aes(x=smoker, y=tip, color=sex))+
+  geom_beeswarm(cex=1)+
+  theme(legend.position = "bottom", 
+        legend.title = element_text(color='blue'))
 
+ggsave("beeswarm.png")
 
 #2 QNT vars: Scatter plot
-g0=ggplot(dtTip, aes(x=total_bill, y=tip))
+g0=ggplot(dtTips, aes(x=total_bill, y=tip))
 g0+geom_point()
 
 #modifications inside geom_point
-g0=ggplot(dtTip, aes(x=total_bill, y=tip))
-g0+geom_point(color=10, shape=1, size=3)
+g0=ggplot(dtTips, aes(x=total_bill, y=tip))
+g0+geom_point(color=10, shape=5, size=2)
 
-names(dtTip)
+names(dtTips)
 #Modifications in aes
-g0=ggplot(dtTip, aes(x=total_bill, y=tip, color=sex))
+g0=ggplot(dtTips, aes(x=total_bill, y=tip, color=sex))
 g0+geom_point()
 
 #Modifications in both 
-g0=ggplot(dtTip, aes(x=total_bill, y=tip, alpha=size))
-g0+geom_point(color=3, size=3)
+g0=ggplot(dtTips, aes(x=total_bill, y=tip, alpha=size))
+g0+geom_point(color=4, size=4)
 
 #Adding a vertical line and size
-g0=ggplot(dtTip, aes(x=total_bill, y=tip,color=time, size=size))
+g0=ggplot(dtTips, aes(x=total_bill, y=tip,color=time, size=size))
 g0+geom_point()+
   geom_vline(xintercept = 40, linetype='dashed')+
   geom_hline(yintercept = 5, linetype='dashed')
 
 #Adding Facet:
+g0=ggplot(dtTips, aes(x=total_bill, y=tip,color=time, size=size))
 g0+geom_point()+
   facet_wrap(~day)
 
-g0=ggplot(dtTip, aes(x=total_bill, y=tip, color=time, size=size))
+g0=ggplot(dtTips, aes(x=total_bill, y=tip, color=time, size=size))
 g0+geom_point()+
   facet_wrap(~smoker)
 
 ##Adding Regression line
-g0=ggplot(dtTip, aes(x=total_bill, y=tip))
+g0=ggplot(dtTips, aes(x=total_bill, y=tip))
 g0+geom_smooth()
 
-g0=ggplot(dtTip, aes(x=total_bill, y=tip, group=sex))
+g0=ggplot(dtTips, aes(x=total_bill, y=tip, group=sex))
 g0+geom_smooth()+
   geom_point()
 
-
+names(dtTips)
 #Adding more detials
-g0=ggplot(dtTip, aes(x=total_bill, y=tip, color=sex))
+g0=ggplot(dtTips, aes(x=total_bill, y=tip, color=sex))
 g1=g0+geom_smooth(se=FALSE)+
-  geom_point(mapping = aes(color=smoker))+
+  geom_point(mapping = aes(color=time))+
   theme_bw()+
   theme(axis.title.x = element_text(), 
         axis.title.y = element_text(), 
